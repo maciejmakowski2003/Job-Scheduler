07aa2d3917335aebf902c6b251c7607987f60a4f
maciejmakowski2003 <maciej.makowski2608@gmail.com>
2026-03-26 19:33:30 +0100

    docs: added doxygen documentation

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -4,23 +4,24 @@
 #include "LoadBalancer.h"
 #include "MpscChannel.hpp"
 #include "Task.h"
+#include "Macros.h"
 #include <memory>
 #include <thread>
 #include <vector>
 
 namespace jobscheduler {
 
+/// @brief A thread pool that manages a fixed number of worker threads to
+/// execute tasks concurrently. It uses load balancer to distribute tasks among
+/// workers and ensures efficient task scheduling.
 class ThreadPool {
 public:
   explicit ThreadPool(size_t numThreads);
+  DELETE_COPY_AND_MOVE(ThreadPool);
   ~ThreadPool();
 
-  ThreadPool(const ThreadPool &) = delete;
-  ThreadPool &operator=(const ThreadPool &) = delete;
-
-  ThreadPool(ThreadPool &&) = delete;
-  ThreadPool &operator=(ThreadPool &&) = delete;
-
+  /// @brief Schedules a task for execution by the thread pool.
+  /// @param task A shared pointer to the task to be executed.
   void schedule(const std::shared_ptr<Task> &task);
 
 private:
@@ -31,6 +32,9 @@ private:
   std::thread loadBalancerThread_;
   std::unique_ptr<MpscChannel<Event>> loadBalancerChannel_;
 
+  /// @brief The worker function that each worker thread runs. It continuously
+  /// listens for events on its channel and executes tasks accordingly.
+  /// @param channel The channel through which the worker receives events.
   void workerFunction(MpscChannel<Event> &channel);
 };
 
