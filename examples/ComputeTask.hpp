0583fe2b4df87885ed381598f6cb06d66c53bdd1
maciejmakowski2003 <maciej.makowski2608@gmail.com>
2026-03-28 11:05:11 +0100

    feat: implemented ping task via udp

--- examples/ComputeTask.hpp
+++ examples/ComputeTask.hpp
@@ -6,12 +6,15 @@
 
 class ComputeTask : public jobscheduler::Task {
 public:
-  explicit ComputeTask(int priority,
-                       std::chrono::milliseconds duration = std::chrono::milliseconds{5000},
-                       std::chrono::system_clock::time_point scheduledTime = std::chrono::system_clock::now())
+  explicit ComputeTask(
+      int priority,
+      std::chrono::milliseconds duration = std::chrono::milliseconds{5000},
+      std::chrono::system_clock::time_point scheduledTime =
+          std::chrono::system_clock::now())
       : Task(scheduledTime, priority, 2), duration_(duration) {}
 
-  bool execute() override {
+protected:
+  bool execute() final {
     std::this_thread::sleep_for(duration_);
     return true;
   }
